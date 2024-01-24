80f7ef0c469877c892d6a1f1ef6be6420cf24432
bernardomw1 <bernardomw@usp.br>
2024-01-23 21:46:46 -0300

    chore: changed projects background image

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,37 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
+
+import CleanCodeImg from '../assets/clean_code.jpg'
+import PostoFluxImg from '../assets/projects/posto_flux.png'
+import DjangoSiteImg from '../assets/projects/DjangoSite.png'
 
 export const data = [
 	{
 		id: 1,
 		name: "Clean Code and Clean Architecture",
-		image: WorkIm,
-		github: "",
+		image: CleanCodeImg,
+		github: "https://github.com/bernardo1mw/Curso_Branas",
 		live: "",
 	},
 	{
 		id: 2,
-		name: "Fuel Station Project",
-		image: RealEst,
-		github: "",
+		name: "Fuel Station Project with Nest and NextJs",
+		image: PostoFluxImg,
+		github: "https://github.com/bernardo1mw/DesafioFluxStation",
 		live: "",
 	},
 	{
 		id: 3,
 		name: "Django Website Project",
-		image: WorkIm,
-		github: "",
+		image: DjangoSiteImg,
+		github: "https://github.com/bernardo1mw/DjangoCourse",
 		live: "",
 	},
-	{
-		id: 4,
-		name: "React JS Application",
-		image: RealEst,
-		github: "",
-		live: "",
-	},
+	// {
+	// 	id: 4,
+	// 	name: "React JS and Spring boot ",
+	// 	image: RealEst,
+	// 	github: "",
+	// 	live: "",
+	// },
 
 
 ]
